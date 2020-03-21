7e526408cff424735402140e06fe4e92a3352f85
Soham <soham.parekh100639@marwadiuniversity.ac.in>
2020-03-21 15:55:49 +0530

    Initialize script added

---
+++ scripts/initialize.sh
@@ -0,0 +1,10 @@
+apt-get -y update
+apt-get -y upgrade
+apt-get install -y git
+apt-get install -y wget
+apt-get install -y openssh
+apt-get install -y nano
+apt-get install -y php
+
+apt-get install -y unstable-repo
+apt-get install -y metasploit
